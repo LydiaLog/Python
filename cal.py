2bc6f562e83e7b34b0a3c18ef85db13b43b15e83
Lydia.Y <110841277+LydiaLog@users.noreply.github.com>
2023-10-10 22:17:45 +0900

    week04
    4주차 컴퓨팅 과제 풀

---
+++ cal.py
@@ -0,0 +1,17 @@
+# 윤서은20215196
+first = int(input("정수 입력 : "))
+cal = str(input("연산자 입력 : " ))
+second = int(input("정수 입력 : "))
+
+if cal == '+' :
+     print(first, cal, second, '=', first + second)
+    
+elif cal == '-' :
+     print(first, cal, second, '=', first - second)
+        
+elif cal == '*' :
+     print(first, cal, second, '=', first * second)
+        
+
+
+    
